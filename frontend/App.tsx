b93243c11be27d75c67869ab6a453d2bd18b819b
Dominic bernas <nickobernas795@gmail.com>
2023-09-02 10:40:02 +0800

    added frontend navigations

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MainScreen from './screens/MainScreen';
+import ProfileScreen from './screens/Profile'
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';//For buttom nav bar just change "RootStack to = createNativeStackNavigator();"
+
+
+export type RootStackParams = {
+  MainScreen: any; 
+  Sports: any
+  Profile: any;
+  Coach: {
+    name: String;
+  }
+}
+const RootStack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootStack.Navigator initialRouteName="MainScreen">
+      <RootStack.Screen name="MainScreen" component={MainScreen} />
+      {/* <RootStack.Screen name="Coach" component={CoachScreen} /> */}
+      <RootStack.Screen name="Profile" component={ProfileScreen} />
+      </RootStack.Navigator>
+    </NavigationContainer>
   );
 }
 
