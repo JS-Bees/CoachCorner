81be09a9e7335fecdc4c677854a89d32ba31aaee
Maj <48152497+Majpotatooo@users.noreply.github.com>
2023-11-07 16:28:05 +0800

    Customer Details Testing

---
+++ frontend/customer_datails_test.dart
@@ -0,0 +1,39 @@
+import '
+
+void main() {
+  group('CustomerDetails', () {
+    group('.isValid()', () {
+      // test('should return false if contact number is invalid.', () {
+      //   final CustomerDetails customerDetails = CustomerDetails(
+      //     uid: '2',
+      //     firstName: 'Justin',
+      //     lastName: 'Grey',
+      //     type: 'Private',
+      //     email: 'justinGrey21@gmail.com',
+      //     phoneNumber: 'testfail',
+      //     landlineNumber: '(033) 320 1212',
+      //     address: const Address(
+      //         addressLine1: 'addressLine1', addressLine2: 'addressLine2'),
+      //   );
+
+      //   expect(customerDetails.isValid(), false);
+      // });
+
+      // test('should return false if contact number is valid.', () {
+      //   final CustomerDetails customerDetails = CustomerDetails(
+      //     uid: '2',
+      //     firstName: 'Justin',
+      //     lastName: 'Grey',
+      //     type: 'Private',
+      //     email: 'justinGrey21@gmail.com',
+      //     phoneNumber: '09259576311',
+      //     landlineNumber: '(033) 320 1212',
+      //     address: const Address(
+      //         addressLine1: 'addressLine1', addressLine2: 'addressLine2'),
+      //   );
+
+      //   expect(customerDetails.isValid(), true);
+      // });
+    });
+  });
+}
