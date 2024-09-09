474d6e0c3b1fcbbc59013900ed2ebe4f9123c386
majpretty <majpretty496@gmail.com>
2024-09-09 09:17:47 +0800

    integration set up

---
+++ frontend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'jest-expo',
+    testEnvironment: 'node',
+    transform: {
+      '^.+\\.(ts|tsx)$': 'ts-jest',
+    },
+    setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  };
